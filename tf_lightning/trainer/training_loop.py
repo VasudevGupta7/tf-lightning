bugs fixed; mixed-precision added

tf_lightning/trainer/training_loop.py
@@ -8,15 +8,14 @@ import tensorflow as tf
 from tf_lightning.callbacks import Callback
 from tf_lightning.loggers import WandbLogger
 from tf_lightning.trainer.checkpointer import Checkpointer
+from tf_lightning.trainer.precision_training import PrecisionTraining
+from tf_lightning.trainer.distributed_training import DistributedTraining
 
 
-class TrainingLoop(Checkpointer):
+class TrainingLoop(Checkpointer, PrecisionTraining, DistributedTraining):
 
     start_epoch = 1
     epochs = 10
-
-    # You can override Callback class and customize methods
-    callbacks = Callback()
 
     # these arguments are valid only for defalt wandb
     # wandb related arguments
@@ -27,10 +26,16 @@ class TrainingLoop(Checkpointer):
 
     log_dir = 'logs'
 
+    # Related to mixed-precision based training
+    policy_name = 'mixed_float16'
+
     # running on only 1 batch for only 1 epoch, No ckpts will be saved
     fast_dev_run = False
 
     def __init__(self):
+
+        # You can override Callback class and customize methods
+        callbacks = Callback()
 
         # Wandb is supported by default
         self.lit_logger = WandbLogger(project_name=project_name,
@@ -40,12 +45,18 @@ class TrainingLoop(Checkpointer):
                                       sync_tensorboard=sync_tensorboard,
                                       save_code=save_code)
 
-        super().__init__()
+        Checkpointer.__init__()
+        PrecisionTraining.__init__(self.policy_name)
 
     def fit(self, lit_module):
 
         # adding methods of lightning-module to trainer
         self.integrate_train_step(lit_module)
+
+        if self.enable_precision_training:
+            self.wrap_mixed_precision_optimizer()
+            self._wrapped_train_step = self._wrapper_precision_train_step
+
         self.wrap_tf_function()
 
     def train(self, tr_dataset, val_dataset):
@@ -85,10 +96,10 @@ class TrainingLoop(Checkpointer):
             if self.save_every_ckpt:
                 self.manager.save()
 
-            # tr_result = self.evaluate(tr_dataset)
-
-            # if bool(self.callbacks):
-            #     epoch_metrics= self.callbacks.on_epoch_end(epoch, tr_result['loss'], val_result['loss'])
+            if bool(self.callbacks):
+                tr_result = self.evaluate(tr_dataset)
+                epoch_metrics = self.callbacks.on_epoch_end(
+                    epoch, tr_result['loss'], val_result['loss'])
 
         if bool(self.callbacks):
             self.callbacks.on_train_end()
@@ -125,23 +136,26 @@ class TrainingLoop(Checkpointer):
         Overwrite this method if necessary else you need not really take care of anything
         """
         for optimizer_idx in self.opt_indices:
+
             result = self.training_step(batch, batch_idx, optimizer_idx)
 
             grads = self.backward(
                 result['loss'], result['trainable_variables'], batch_idx, optimizer_idx)
+
             self.optimizer_step(grads, trainable_variables,
                                 batch_idx, optimizer_idx)
 
         return result
 
     def integrate_train_step(self, lit_module):
+        # this will run only once
         # called inside fit method
 
         self.training_step = lit_module.training_step
         self.val_step = lit_module.val_step
 
         optimizer, opt_indices = self._get_optimizer(lit_module)
-        self.optimizer_indices = optimizer_indices
+        self.opt_indices = opt_indices
         for i in opt_indices:
             setattr(self, f"optimizer_{i}", optimizer[i])
 
@@ -155,15 +169,8 @@ class TrainingLoop(Checkpointer):
 
         if isinstance(optimizers, tf.keras.optimizers.Optimizer):
             opt_indices = [0]
-            return optimizers, opt_indices
 
-        elif isinstance(optimizers, tuple):
+        elif isinstance(optimizers, (tuple, list)):
+            opt_indices = list(range(len(optimizers)))
 
-            if len(optimizers) == 1:
-                opt_indices = [0]
-                optimizer = optimizers[0]
-                return optimizer, opt_indices
-
-            else:
-                opt_indices = list(range(len(optimizers)))
-            return optimizers, opt_indices
+        return optimizers, opt_indices